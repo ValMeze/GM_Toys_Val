8729347ba796f78d6b71d5136ef3264957ff6216
ValMeze <157593975+ValMeze@users.noreply.github.com>
2024-11-07 22:31:01 +0100

    back off.js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -8,22 +8,20 @@ const renderBadge = () => {
 const listenToAdd = (buttons) => {
   buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
-      const { id, name, price } = event.currentTarget.dataset
+      const { id, name, price, location } = event.currentTarget.dataset
       if (cartLS.exists(id)) {
         cartLS.quantity(id, 1)
       } else {
         cartLS.add({ id, name, price })
       }
       dataLayer.push({
-        event: 'add_to_cart',
-        product_id: id,
-        product_name: name,
-        product_price: Number(price)
+        event: 'addToCart',
+        item: { id, name, price },
+        location: location
       })
     })
   })
-}
-
+  
 const cartItemsListeners = () => {
   const addToCartButtons = document.querySelectorAll('.cart .add-to-cart')
   listenToAdd(addToCartButtons)
